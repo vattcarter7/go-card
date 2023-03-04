6749e975251fefd7c2ecac3eb3296b49f382fb4b
Vatt Sopheak <vattcarter@gmail.com>
2023-03-04 14:34:12 +0700

    Populated new deck of cards and modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Go project
+
+> Simple card project written in golang 
--- deck.go
+++ deck.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
