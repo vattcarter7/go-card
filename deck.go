2c5b2593d6a7b6267b1b5b2918ee754452541a73
Vatt Sopheak <vattcarter@gmail.com>
2023-03-05 11:39:31 +0700

    Implemented shuffle func with random

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -47,12 +49,22 @@ func (d deck) saveToFile(filename string) error {
 func newDeckFromFile(filename string) deck {
 	bs, err := ioutil.ReadFile(filename)
 	if err != nil {
-		// Option #1 - log the error and return a call to newDeck()
-		// Option #2 - log the error and entirely quit the program
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		// swap position
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
